d77ee8a2f6416ffd6d179411b2031ad6297e4099
Gabriel Evaristo <gabrielevaristovcp@gmail.com>
2021-09-15 10:26:25 -0300

    Finalizando Api

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
  
 module.exports = () => {
  const app = express()
- 
+//  "?"
  app.use(bodyParser.json())
  app.use(bodyParser.urlencoded({ extended: true }))
  
